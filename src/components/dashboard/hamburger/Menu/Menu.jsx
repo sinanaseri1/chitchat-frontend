2a6b5edf6badf5b7e8420b1fe386fc4169015dca
Sina <naseri.sina@hotmail.com>
2025-02-10 12:11:44 +0000

    fixed burger menu

--- src/components/dashboard/hamburger/Menu/Menu.jsx
+++ src/components/dashboard/hamburger/Menu/Menu.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-
 import ListItem from './ListItem'
 
 const Menu = () => {
-    return (
-			<div 
-        className='absolute w-[max-content] min-w-[240px] border-solid border-white border-x-4 shadow-lg'
-			>
-				<ul>
-					<ListItem name='Add User' />
-					<ListItem name='Delete User' />
-					<ListItem name='Block User' />
-				</ul>
+  return (
+    <div
+      // Added top/right positioning, a background color, and z-index for visibility
+      className='absolute top-[60px] right-4 w-[max-content] min-w-[240px] bg-white border-solid border-white border-x-4 shadow-lg z-50'
+    >
+      <ul>
+        <ListItem name='Add User' />
+        <ListItem name='Delete User' />
+        <ListItem name='Block User' />
+      </ul>
     </div>
-	)
+  )
 }
 
 export default Menu
