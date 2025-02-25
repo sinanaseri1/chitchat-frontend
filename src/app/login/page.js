689508e75e9274f3d4426d909eaf5695472cb817
Martina Best <martinabest@mac.home>
2025-02-25 16:14:23 +0000

    mobile 3

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
-import { isMobile } from "../utils/isMobile"; // Utility function to detect mobile
+import { isMobile } from "@/utils/isMobile";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001";
 
